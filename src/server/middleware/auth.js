293ffbebaf344d73ec524851f6a854db8f76de51
NestZ <NestZ671@gmail.com>
2020-04-05 18:19:16 +0700

    add schema

--- src/server/middleware/auth.js
+++ src/server/middleware/auth.js
@@ -2,7 +2,18 @@ const jwt = require('jsonwebtoken');
 const Student = require('../models/studentModel');
 
 const auth = async (req, res, next) => {
-
+    try{
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const payload = jwt.verify(token, process.env.TOKEN_KEY);
+        const student = await Student.findOne({_id : payload._id, 'tokens.token' : token});
+        if(!student)throw new Error();
+        req.student = student;
+        req.token = token;
+        next();
+    }
+    catch(error){
+        res.status(401).json({error : error.message});
+    }
 }
 
 module.exports = auth;
---
+++ src/server/models/studentModel.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const validator = require('validator')
+
+const studentSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        validator : (value) => {
+            if(!validator.isEmail()){
+                throw new Error('Invalid email !');
+            }
+        }
+    },
+    password : {
+        type : String,
+        required : true,
+        minlength : 6
+    },
+    tokens : [{
+        token : {type : String, required : true}
+    }],
+});
+
+studentSchema.pre('save', async function(next){
+    const student = this;
+    if(student.isModified('password')){
+        student.password = await bcrypt.hash(student.password, 10);
+    }
+    next();
+});
+
+studentSchema.methods.generateAuthToken = async function(){
+    const student = this;
+    const payload = {
+        _id : student._id,
+        email : student.email,
+        admin : student.admin
+    };
+
+    const token = jwt.sign(payload, process.env.TOKEN_KEY, {expiresIn : '2h', issuer : 'CMU'});
+    student.tokens = student.tokens.concat({token});
+    await student.save();
+    return token;
+}
+
+studentSchema.statics.findByCredentials = async (email, password) => {
+    try{
+        const student = await Student.findOne({email});
+        if(!student)throw new Error({message : "Student not found!"});
+        const isPasswordMatch = await bcrypt.compare(password, student.password);
+        if(!isPasswordMatch)throw new Error({message : 'Password not match!'});
+        return student;
+    }
+    catch(error){
+        return null;
+    }
+}
+
+const Student = mongoose.model('Student', studentSchema);
+module.exports = Student;
