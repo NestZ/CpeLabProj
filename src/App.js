cb261f2d4d8b5a0a358a6e2b9312f3d5223e32cf
tee <semada15@gmail.com>
2020-04-08 15:07:39 +0700

    Mainmenu_no_css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import MainMenu from './components/Main_menu'
+import Studentlist from './components/Studentlist'
+import Testtable from './components/Testtable'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+      <Route exact path="/" render={ props => (
+          <div>
+            <MainMenu></MainMenu>
+          </div>
+        )} />
+
+
+        <Route path="/mainmenu" component={MainMenu} />
+        <Route path="/Studentlist" component={Studentlist} />
+        <Route path="/Testtable" component={Testtable}/>      
+      </div>
+    </Router>
   );
 }
 
