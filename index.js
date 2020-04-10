7e8c65b30b21b19a5b676ffc116c829dfc35ae4f
NestZ <NestZ671@gmail.com>
2020-04-10 13:29:38 +0700

    :)

--- index.js
+++ index.js
@@ -2,21 +2,21 @@
 require('dotenv').config();
 const express = require('express');
 const bp = require('body-parser');
-const exp = express();
+const app = express();
 const path = require('path');
 const Router = require('./routes/router');
 var cors = require('cors');
 
-exp.use(cors());
-exp.use(bp.json());
-exp.use(bp.urlencoded({extended : true}));
+app.use(cors());
+app.use(bp.json());
+app.use(bp.urlencoded({extended : true}));
 // exp.use(express.static(path.join(__dirname, 'build')));
 app.use(express.static(path.join(__dirname, 'client/build')));
-exp.use(Router);
+app.use(Router);
 
 const PORT = process.env.PORT;
 const HOSTNAME = '0.0.0.0';
 
-exp.listen(PORT || 5000, HOSTNAME, () => {
+app.listen(PORT || 5000, HOSTNAME, () => {
     console.log('server is running at port : ' + PORT);
 });
