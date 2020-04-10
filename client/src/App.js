c39fedd7f9da1439ff6dd5a391f468997d830d6c
NestZ <NestZ671@gmail.com>
2020-04-10 16:37:18 +0700

    :)

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import MainMenu from './components/MainMenu'
 import Course from './components/Course'
 import Login from './components/Login'
 import Register from './components/Register'
-import Studentlist from './components/Studentlist';
 import Drop from './components/Drop'
 import Profile from './components/Profile'
 import RequireAuth from './components/RequireAuth'
