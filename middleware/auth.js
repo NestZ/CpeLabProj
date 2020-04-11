d6c2921cef7fb23644e3589c9e73ca099f296aa7
NestZ <NestZ671@gmail.com>
2020-04-11 16:25:53 +0700

    :)

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const Student = require('../models/studentModel');
 
 const auth = async (req, res, next) => {
     try{
-        const token = sessionStorage.getItem('token').replace('Bearer', '');
+        const token = sessionStorage.getItem('token').replace('Bearer ', '');
         if(token == null)token = req.header('Authorization').replace('Bearer ', '');
         const payload = jwt.verify(token, 'reg-cmu');
         const student = await Student.findOne({_id : payload._id, 'tokens.token' : token});
