676c6d45c41ae94db90d7b8168457de83bc6cd71
NestZ <NestZ671@gmail.com>
2020-04-11 19:56:36 +0700

    :)

--- index.js
+++ index.js
@@ -13,8 +13,16 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(Router);
 
 const PORT = process.env.PORT;
-const HOSTNAME = '0.0.0.0';
+// const HOSTNAME = '0.0.0.0';
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
-app.listen(PORT || 5000, HOSTNAME, () => {
+app.listen(PORT || 5000, () => {
     console.log('server is running at port : ' + PORT);
 });
